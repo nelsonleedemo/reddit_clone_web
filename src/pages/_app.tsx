0b63ad47e4a32fd5a092cb8abeacdd3dfd08158b
nelsonlee <95209960+nelsonleedemo@users.noreply.github.com>
2021-12-26 00:55:23 +0800

    1. Create urql client, preparing for server-side rendering 2. Show posts in home page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,91 +3,11 @@ import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../theme";
 import { AppProps } from "next/app";
 
-import { Provider, createClient, dedupExchange, fetchExchange } from "urql";
-import { Cache, cacheExchange, QueryInput } from "@urql/exchange-graphcache";
-import {
-  LoginMutation,
-  LogoutMutation,
-  MeDocument,
-  MeQuery,
-  RegisterMutation,
-} from "../generated/graphql";
-
-function betterUpdateQuery<Result, Query>(
-  cache: Cache,
-  qi: QueryInput,
-  result: any,
-  fn: (r: Result, q: Query) => Query
-) {
-  return cache.updateQuery(qi, (data) => fn(result, data as any) as any);
-}
-
-const client = createClient({
-  url: "http://localhost:4000/graphql",
-  // url: "https://studio.apollographql.com", // localhost server listening to port 4000 but not to studio.apollographql.com
-  fetchOptions: {
-    credentials: "include",
-  },
-  exchanges: [
-    dedupExchange,
-    cacheExchange({
-      updates: {
-        Mutation: {
-          login: (_result, args, cache, info) => {
-            // cache.updateQuery({ query: MeDocument }, (data: MeQuery) => {});
-            betterUpdateQuery<LoginMutation, MeQuery>(
-              cache,
-              { query: MeDocument },
-              _result,
-              (result, query) => {
-                if (result.login.errors) {
-                  return query;
-                } else {
-                  return {
-                    me: result.login.user,
-                  };
-                }
-              }
-            );
-          },
-          register: (_result, args, cache, info) => {
-            betterUpdateQuery<RegisterMutation, MeQuery>(
-              cache,
-              { query: MeDocument },
-              _result,
-              (result, query) => {
-                if (result.register.errors) {
-                  return query;
-                } else {
-                  return {
-                    me: result.register.user,
-                  };
-                }
-              }
-            );
-          },
-          logout: (_result, args, cache, info) => {
-            betterUpdateQuery<LoginMutation, MeQuery>(
-              cache,
-              { query: MeDocument },
-              _result,
-              () => ({ me: null })
-            );
-          },
-        },
-      },
-    }),
-    fetchExchange,
-  ],
-});
-
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Provider value={client}>
-      <ChakraProvider resetCSS theme={theme}>
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </Provider>
+    <ChakraProvider resetCSS theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
   );
 }
 
---
+++ src/utils/createUrqlClient.ts
@@ -0,0 +1,69 @@
+import { cacheExchange } from "@urql/exchange-graphcache";
+import { dedupExchange, fetchExchange } from "urql";
+import {
+  LoginMutation,
+  MeQuery,
+  MeDocument,
+  RegisterMutation,
+} from "../generated/graphql";
+import { betterUpdateQuery } from "./betterUpdateQuery";
+
+export const createUrqlClient = (ssrExchange: any) => ({
+  url: "http://localhost:4000/graphql",
+  // url: "https://studio.apollographql.com", // localhost server listening to port 4000 but not to studio.apollographql.com
+  fetchOptions: {
+    credentials: "include" as const,
+  },
+  exchanges: [
+    dedupExchange,
+    cacheExchange({
+      updates: {
+        Mutation: {
+          login: (_result, args, cache, info) => {
+            // cache.updateQuery({ query: MeDocument }, (data: MeQuery) => {});
+            betterUpdateQuery<LoginMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              _result,
+              (result, query) => {
+                if (result.login.errors) {
+                  return query;
+                } else {
+                  return {
+                    me: result.login.user,
+                  };
+                }
+              }
+            );
+          },
+          register: (_result, args, cache, info) => {
+            betterUpdateQuery<RegisterMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              _result,
+              (result, query) => {
+                if (result.register.errors) {
+                  return query;
+                } else {
+                  return {
+                    me: result.register.user,
+                  };
+                }
+              }
+            );
+          },
+          logout: (_result, args, cache, info) => {
+            betterUpdateQuery<LoginMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              _result,
+              () => ({ me: null })
+            );
+          },
+        },
+      },
+    }),
+    fetchExchange,
+    ssrExchange,
+  ],
+});
